e64a40c117b9cfbd788d488ebec76c18fbba4134
fqidz <faidz.arante@gmail.com>
2025-10-04 12:12:50 +0300

    feat: add lebron

--- Frontend/Nasa_WINATHON/src/App.jsx
+++ Frontend/Nasa_WINATHON/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from "./components/theme-provider"
+import { Button } from "@/components/ui/button"
+import lebron from './assets/lebron.png'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Button variant="outline">Button</Button>
+            <img src={lebron} alt="Lebron holding a meteor on one hand, and a
+            basketball on the other hand, with one leg on top of a basketball">
+            </img>
+        </ThemeProvider>
+    )
 }
-
-export default App
